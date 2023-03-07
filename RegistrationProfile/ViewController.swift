23dc544fc946e2fed5e202b2472514c6809cb9c3
snzmxm <mxmdev96@gmail.com>
2023-03-07 17:25:35 +0300

    Initial Commit

---
+++ RegistrationProfile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RegistrationProfile
+//
+//  Created by SNZ on 07.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
